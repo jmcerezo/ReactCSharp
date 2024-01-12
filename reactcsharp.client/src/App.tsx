b6ab86dc4014f3354217ddda2f2ed8224d76d9c3
JM Cerezo <johnmenardccerezo@gmail.com>
2024-01-12 14:36:27 +0800

    Add input validations

--- reactcsharp.client/src/App.tsx
+++ reactcsharp.client/src/App.tsx
@@ -1,25 +1,125 @@
-import { useState } from 'react';
-import axios from "axios"
+import {  useState } from 'react';
+import axios from 'axios';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './App.css';
 
+
 function App() {
     const [firstName, setFirstName] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
     const [lastName, setLastName] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
     const [emailAddress, setEmailAddress] = useState("");
+    const [emailAddressError, setEmailAddressError] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
+    const [phoneNumberError, setPhoneNumberError] = useState("");
+    const alphabetsRegex = /^[a-zA-Z ]*$/
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
+    const phoneRegex = /^!*([0-9]!*){10,}$/
+
+    const validateFirstName = (name: string) => {
+        let isValid;
+
+        if (name.trim() === "") {
+            setFirstNameError("This is required");
+            isValid = false;
+        } else if (!alphabetsRegex.test(name)) {
+            setFirstNameError("Enter alphabets only")
+            isValid = false;
+        } else if (name.trim().length < 2) {
+            setFirstNameError("Enter at least 2 characters")
+            isValid = false;
+        } else {
+            setFirstNameError("")
+            isValid = true;
+        }
+
+        return isValid
+    }
+
+    const validateLastName = (name: string) => {
+        let isValid;
+
+        if (name.trim() === "") {
+            setLastNameError("This is required");
+            isValid = false;
+        } else if (!alphabetsRegex.test(name)) {
+            setLastNameError("Enter alphabets only")
+            isValid = false;
+        } else if (name.trim().length < 2) {
+            setLastNameError("Enter at least 2 characters")
+            isValid = false;
+        } else {
+            setLastNameError("")
+            isValid = true;
+        }
+
+        return isValid
+    }
+
+    const validateEmailAddress = (email: string) => {
+        let isValid;
+
+        if (email.trim() === "") {
+            setEmailAddressError("This is required");
+            isValid = false;
+        } else if (!emailRegex.test(email)) {
+            setEmailAddressError("Enter valid email address")
+            isValid = false;
+        } else {
+            setEmailAddressError("")
+            isValid = true;
+        }
+
+        return isValid
+    }
+
+    const validatePhoneNumber = (phone: string) => {
+        let isValid;
+
+        if (!phone) {
+            setPhoneNumberError("This is required");
+            isValid = false;
+        } else if (!phoneRegex.test(phone)) {
+            setPhoneNumberError("Enter at least 10 digits")
+            isValid = false;
+        } else {
+            setPhoneNumberError("")
+            isValid = true;
+        }
+
+        return isValid
+    }
+
+    const validateForm = () => {
+        validateFirstName(firstName)
+        validateLastName(lastName)
+        validateEmailAddress(emailAddress)
+        validatePhoneNumber(phoneNumber)
+
+        return (
+            validateFirstName(firstName) &&
+            validateLastName(lastName) &&
+            validateEmailAddress(emailAddress) &&
+            validatePhoneNumber(phoneNumber)
+        )
+    }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.id === "firstName") {
-            setFirstName(e.target.value);
+            setFirstName(e.target.value)
+            validateFirstName(e.target.value)
         } else if (e.target.id === "lastName") {
-            setLastName(e.target.value);
+            setLastName(e.target.value)
+            validateLastName(e.target.value)
         } else if (e.target.id === "emailAddress") {
-            setEmailAddress(e.target.value);
+            setEmailAddress(e.target.value)
+            validateEmailAddress(e.target.value)
         } else if (e.target.id === "phoneNumber") {
-            setPhoneNumber(e.target.value);
+            setPhoneNumber(e.target.value)
+            validatePhoneNumber(e.target.value)
         }
     }
 
@@ -27,21 +127,19 @@ function App() {
         e.preventDefault();
 
         const data = { firstName, lastName, emailAddress, phoneNumber };
+        const formIsValid = validateForm()
 
-        if (firstName && lastName && emailAddress && phoneNumber) {
-            console.log("DATA: ", data);
-
+        if (formIsValid) {
             axios.post("https://localhost:7154/api/Users", data).then((res) => {
-                console.log("RESPONSE: ", res)
+                console.log(res.status)
 
                 setFirstName("")
                 setLastName("")
                 setEmailAddress("")
                 setPhoneNumber("")
+
                 alert("Successfully registered")
             })
-        } else {
-            alert("Please fill out all required fields")
         }
     }
 
@@ -58,10 +156,10 @@ function App() {
                 autoComplete="off"
                 onSubmit={handleSubmit}
             >
-                <TextField id="firstName" label="First Name" variant="outlined" fullWidth required value={firstName} onChange={handleChange} />
-                <TextField id="lastName" label="Last Name" variant="outlined" fullWidth required value={lastName} onChange={handleChange} />
-                <TextField id="emailAddress" label="Email Address" variant="outlined" fullWidth required value={emailAddress} onChange={handleChange} />
-                <TextField id="phoneNumber" label="Phone Number" variant="outlined" type="number" fullWidth required value={phoneNumber} onChange={handleChange} />
+                <TextField id="firstName" label="First Name" variant="outlined" required fullWidth value={firstName} helperText={firstNameError} onChange={handleChange} />
+                <TextField id="lastName" label="Last Name" variant="outlined" required fullWidth value={lastName} helperText={lastNameError} onChange={handleChange} />
+                <TextField id="emailAddress" label="Email Address" variant="outlined" required fullWidth value={emailAddress} helperText={emailAddressError} onChange={handleChange} />
+                <TextField id="phoneNumber" label="Phone Number" variant="outlined" type="number" required fullWidth value={phoneNumber} helperText={phoneNumberError} onChange={handleChange} />
                 <Button variant="contained" type="submit">Submit</Button>
             </Box>
         </div>
